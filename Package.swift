4442b0003996bab89c77b669861514c934566f38
Borut Tomazin <borut@povio.com>
2025-10-06 08:49:33 +0200

    Version update.

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/google/GoogleSignIn-iOS", .upToNextMajor(from: "9.0.0")),
-    .package(url: "https://github.com/povio/PovioKitAuth", branch: "release/3.0.0")
+    .package(url: "https://github.com/povio/PovioKitAuth", .upToNextMajor(from: "3.0.0"))
   ],
   targets: [
     .target(
